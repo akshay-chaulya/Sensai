850f679b568a6424765178a8225b61ac9c36da0b
akshay08test <akshaychaulya08@gmail.com>
2025-03-01 23:10:14 +0530

    deploy error check and fixing

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,15 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 import Header from "@/components/header";
+import { ThemeProvider } from "@/components/theme-provider";
 import { dark } from "@clerk/themes";
-import { Toaster } from "@/components/ui/sonner";
 
-const inter = Inter({
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Sensai - AI career coach",
+  title: "AI Career Coach",
   description: "",
 };
 
@@ -23,16 +21,23 @@ export default function RootLayout({ children }) {
       }}
     >
       <html lang="en" suppressHydrationWarning>
+        <head>
+          <link rel="icon" href="/logo.png" sizes="any" />
+        </head>
         <body className={`${inter.className}`}>
-          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-            {/* header */}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
             <Header />
             <main className="min-h-screen">{children}</main>
             <Toaster richColors />
-            {/* footer */}
+
             <footer className="bg-muted/50 py-12">
-              <div className="container mx-auto px-4 text-center">
-                <p>Made by akshay</p>
+              <div className="container mx-auto px-4 text-center text-gray-200">
+                <p>Made with 💗 by Akshay</p>
               </div>
             </footer>
           </ThemeProvider>
